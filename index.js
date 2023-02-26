a220a510ba250c25c6623b6991ed97da7858261f
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2023-02-26 19:48:49 +0545

    added scroll to top

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function toggleMenu(){
 }
 
 hamburger.addEventListener('click', toggleMenu)
+
+
+let mybutton = document.querySelector(".scroll-to-top");
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
